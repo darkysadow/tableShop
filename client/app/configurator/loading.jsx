1045ec8d004d14620b4fa253b8b04061285ce93f
darkysadow <alexsmithshow@gmail.com>
2024-02-16 02:43:51 +0200

    Added loading page for configurator

---
+++ client/app/configurator/loading.jsx
@@ -0,0 +1,12 @@
+import ImagePreloader from "@/components/Preloaders/ImagePreloader";
+
+export default function ConfiguratorPageLoading() {
+    return (
+        <div className="min-h-[calc(100vh-86px)] bg-white flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                <ImagePreloader />
+                <h1 className="text-center">Loading models and textures...</h1>
+            </div>
+        </div>
+    )
+}
